797840f652269151f84076cd604b7817e5e14a07
ThangVT <thangthang21599@gmail.com>
2025-04-22 16:03:24 +0700

    UPDATE - update app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'FLS POD Ticket App',
+  description: 'Created by thangvt21',
   generator: 'v0.dev',
 }
 
